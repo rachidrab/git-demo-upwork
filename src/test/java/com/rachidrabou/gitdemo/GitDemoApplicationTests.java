d4da11a2d678cb59069f03cd6d794105e303f433
rachidrabou <rachidrabou4@gmail.com>
2021-04-21 02:11:20 +0000

    initial commit

---
+++ src/test/java/com/rachidrabou/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachidrabou.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
